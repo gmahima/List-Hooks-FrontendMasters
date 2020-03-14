0fbcf9ab36afd11c1502322b1ccf32fe478d64cf
gmahima <mahimagangavarapu@gmail.com>
2020-03-14 20:22:55 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useReducer, useState } from 'react';
+
+import id from 'uuid/v4';
+
+import Grudges from './Grudges';
+import NewGrudge from './NewGrudge';
+
+import initialState from './initialState';
+
+const GRUDGE_ADD = 'GRUDGE_ADD';
+const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE';
+
+const reducer = (state, action) => {
+  return state
+}
+
+const App = () => {
+  const [grudges, dispatch] = useReducer(reducer, initialState)
+
+
+
+
+  const toggleForgiveness = id => {
+    // setGrudges(
+    //   grudges.map(grudge => {
+    //     if (grudge.id !== id) return grudge;
+    //     return { ...grudge, forgiven: !grudge.forgiven };
+    //   })
+    // );
+  };
+
+  return (
+    <div className="Application">
+      <NewGrudge onSubmit={addGrudge} />
+      <Grudges grudges={grudges} onForgive={toggleForgiveness} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/NewGrudge.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const NewGrudge = ({ onSubmit }) => {
+  const [person, setPerson] = useState('');
+  const [reason, setReason] = useState('');
+
+  console.log('Rendering New Grudge');
+
+  const handleChange = event => {
+    event.preventDefault();
+    onSubmit({ person, reason });
+  };
+
+  return (
+    <form className="NewGrudge" onSubmit={handleChange}>
+      <input
+        className="NewGrudge-input"
+        placeholder="Person"
+        type="text"
+        value={person}
+        onChange={event => setPerson(event.target.value)}
+      />
+      <input
+        className="NewGrudge-input"
+        placeholder="Reason"
+        type="text"
+        value={reason}
+        onChange={event => setReason(event.target.value)}
+      />
+      <input className="NewGrudge-submit button" type="submit" />
+    </form>
+  );
+};
+
+export default NewGrudge;
